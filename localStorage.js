4df165967f34b5474b9696d68a0bc98fae23b760
codelibra <shivendranitb@gmail.com>
2015-06-02 01:19:41 +0530

    Bug fix

--- localStorage.js
+++ localStorage.js
@@ -1,4 +1,4 @@
-ta/**
+/**
  * This script will be running  in the background and will collect the complete information
  * about what all pages the user visits and using what search terms.
  * Then it will generate some data structure which will be used to search in future the related pages
